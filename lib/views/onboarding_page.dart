6b251abe723db46f646e13482728e413aa11d5c3
Kuldip Rajput <kuldip.r@appstute.in>
2022-03-01 20:20:57 +0530

    first commit

---
+++ lib/views/onboarding_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_onboarding_demo/controllers/onboarding_controller.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+class OnboardingPage extends StatelessWidget {
+  final _controller = OnboardingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            PageView.builder(
+                controller: _controller.pageController,
+                onPageChanged: _controller.selectedPageIndex,
+                itemCount: _controller.onboardingPages.length,
+                itemBuilder: (context, index) {
+              return Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(_controller.onboardingPages[index].imageAsset),
+                    SizedBox(height: 32,),
+                    Text(_controller.onboardingPages[index].title, style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),),
+                    SizedBox(height: 32,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 58),
+                      child: Text(_controller.onboardingPages[index].description,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 18)),
+                    ),
+                  ],
+                ),
+              );
+            }),
+            Positioned(
+              bottom: 20,
+              left: 20,
+              child: Row(
+                children: List.generate(
+                        _controller.onboardingPages.length,
+                        (index) => Obx(() {
+                            return Container(
+                              margin: const EdgeInsets.all(4),
+                              height: 12,
+                              width: 12,
+                              decoration: BoxDecoration(
+                                  color: _controller.selectedPageIndex.value == index
+                                      ? Colors.red
+                                      : Colors.grey,
+                                  shape: BoxShape.circle
+                              ),
+                            );
+                          }
+                        )),
+              ),
+            ),
+            Positioned(
+                bottom: 20,
+                right: 20,
+                child: FloatingActionButton(
+                  elevation: 0,
+                  onPressed: _controller.forwardAction,
+                  child: Obx(() {
+                      return Text(_controller.isLastPage ? 'Start' : 'Next');
+                    }
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
